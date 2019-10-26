c94be97e952edab9affa59e49ff061d914ab52eb
Pierre Chalamet <pierre@chalamet.net>
2019-10-26 14:04:03 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app" class="container">
+        <img alt="Vue logo" src="./assets/logo.png" width="10%">
+        <b-field>
+            <b-input v-model="newtodo" @ />
+            <b-button type="is-success" :disabled="newtodo === ''" @click="addTodo">
+                <b-icon icon="plus" />
+            </b-button>
+        </b-field>
+        <transition-group name="fade">
+            <b-field v-for="todo in todos" :key="todo.id" >
+                <ToDoItem :todo="todo" @delete="deleteTodo" />
+            </b-field>
+        </transition-group>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoItem from './components/ToDoItem.vue'
+import { ToastProgrammatic as Toast } from 'buefy'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    data() {
+        return { 
+            newtodo: "",
+            todoid: 0,
+            todos: [ ]
+        }
+    },
+    components: {
+        ToDoItem
+    },
+    methods: {
+        addTodo() {
+            const currtodo = { text: this.newtodo, id: this.todoid }
+            this.todoid = this.todoid + 1
+            this.newtodo = ""
+
+            this.todos.push(currtodo)
+            Toast.open({message: 'Todo added !', type: 'is-success', position: 'is-bottom'})
+        },
+
+        deleteTodo(todo) {
+            this.todos = this.todos.filter(item => item !== todo)
+            Toast.open({message: 'Todo deleted !', type: 'is-danger', position: 'is-bottom'})
+        }
+    }
 }
 </script>
 
@@ -25,4 +58,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <b-field>
+            <b-input :value="todo.text" readonly="true" />
+            <b-button type="is-danger" @click="deleteTodo">
+                <b-icon icon="close" />
+            </b-button>
+        </b-field>
+    </div>
+</template>
+
+
+<script>
+
+export default {
+    props: ['todo', 'onDelete'],
+    methods: {
+        deleteTodo() {
+            this.$emit('delete', this.todo)
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
